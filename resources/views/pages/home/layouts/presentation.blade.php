bbb650d514def4b43e6e22ebdb79206c30f631ff
Felipe Sales <felipesales007@hotmail.com>
2020-05-09 17:39:16 -0300

    remoção de tag em home

--- resources/views/pages/home/layouts/presentation.blade.php
+++ resources/views/pages/home/layouts/presentation.blade.php
@@ -3,7 +3,6 @@
         <div class="container">
             <div class="row align-items-center justify-content-center text-center">
                 <div class="col-md-10">
-                    <span class="d-inline-block bg-success text-white px-3 mb-3 property-offer-type rounded">Lançamento</span>
                     <h1 class="mb-2">Condomínio Varandas do Vale</h1>
                     <p class="mb-5"><strong class="h2 text-success font-weight-bold">R$ 250.000</strong></p>
                     <p><a href="javascript:void(0)" class="btn btn-white btn-outline-white py-3 px-5 btn-2">Mais Detalhes</a></p>
@@ -16,7 +15,6 @@
         <div class="container">
             <div class="row align-items-center justify-content-center text-center">
                 <div class="col-md-10">
-                    <span class="d-inline-block bg-warning text-white px-3 mb-3 property-offer-type rounded">Aluguel</span>
                     <h1 class="mb-2">Condomínio Flores do Campo</h1>
                     <p class="mb-5"><strong class="h2 text-success font-weight-bold">R$ 1.200</strong></p>
                     <p><a href="javascript:void(0)" class="btn btn-white btn-outline-white py-3 px-5 btn-2">Mais Detalhes</a></p>
@@ -29,7 +27,6 @@
         <div class="container">
             <div class="row align-items-center justify-content-center text-center">
                 <div class="col-md-10">
-                    <span class="d-inline-block bg-info text-white px-3 mb-3 property-offer-type rounded">Usado</span>
                     <h1 class="mb-2">Condomínio Cores de Piatã</h1>
                     <p class="mb-5"><strong class="h2 text-success font-weight-bold">R$ 220.000</strong></p>
                     <p><a href="javascript:void(0)" class="btn btn-white btn-outline-white py-3 px-5 btn-2">Mais Detalhes</a></p>
